6304e4581c8822b4a253bd00f07276a88b3aaa79
Julien Lavergne <julien.lavergne@gmail.com>
2014-05-15 14:45:58 +0200

    Adjustements to make it dual building for qt4 and qt5, with common headers

---
+++ cmake/qtxdg-qt4_use.cmake
@@ -0,0 +1,10 @@
+# - Find the Razor-qt include and library dirs and define a some macros
+#
+
+
+find_package(Qt4 REQUIRED QUIET)
+include(${QT_USE_FILE})
+
+set(QTXDG_QT_LIBRARIES ${QT_LIBRARIES})
+
+include_directories(${QTXDG_INCLUDE_DIRS})
